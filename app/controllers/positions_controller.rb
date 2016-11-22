32f6a47619db3363104b7b29bbaa4c7f55efa26c
armandnaregian <armandnaregian@hotmail.com>
2016-11-22 01:25:40 -0500

    added comments

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -1,25 +1,20 @@
+# Controller for the positions
+
 class PositionsController < ApplicationController
+	# Calls the set_position method before executing edit, update, show or destroy actions
 	before_action :set_position, only: [:edit, :update, :show, :destroy]
+
+	# Controls the positions list page
 	def index
 		@positions = Position.all
 	end
+
+	# Controls the process of creating a new position
 	def new
 		@position = Position.new
 	end
 
-	def edit
-
-	end
-
-	def update
-		if @position.update(position_params)
-			flash[:success] = "Position updated"
-			redirect_to position_path(@position)
-		else
-			render 'edit'
-		end
-	end
-
+	# Creates the new position
 	def create
 		@position = Position.new(position_params)
 		if @position.save
@@ -30,20 +25,42 @@ class PositionsController < ApplicationController
 		end
 	end
 
+	# Controls the process of editing an existing position
+	def edit
+
+	end
+
+	# Updates the edited position
+	def update
+		if @position.update(position_params)
+			flash[:success] = "Position updated"
+			redirect_to position_path(@position)
+		else
+			render 'edit'
+		end
+	end
+
+	# Controls the show page of an individual position
 	def show
 
 	end
 
+	# Deletes a position
 	def destroy
 		@position.destroy
 		flash[:danger] = "Position was deleted"
 		redirect_to positions_path
 	end
 
+	# Private methods
 	private
+
+	# Sets the position variable to the selected position
 	def set_position
 		@position = Position.find(params[:id])
 	end
+
+	# Assigns the parameters entered while creating or editing a position
 	def position_params
 		params.require(:position).permit(:name, :allocated_money)
 	end
--- app/models/position.rb
+++ app/models/position.rb
@@ -1,4 +1,8 @@
+# Model for the positions
 class Position < ActiveRecord::Base
+	# Validates the presence and the length of the name colummn
 	validates :name, presence: true, length: {minimum: 2, maximum: 25}
+
+	# Validates the presence and the numericality of the allocated_money column
 	validates :allocated_money, presence: true, numericality: {greater_than_or_equal_to: 0}
 end
