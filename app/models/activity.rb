d8d64ddc16a158d8056948ce5a1f167cbdbe97d2
armandnaregian <armandnaregian@hotmail.com>
2016-11-22 20:06:10 -0500

    added activity validations

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,7 @@
 class Activity < ActiveRecord::Base
+	# Validates the presence and the length of the name colummn
+	validates :activity_name, presence: true, length: {minimum: 2, maximum: 25}
 
+	# Validates the presence and the numericality of the allocated_money column
+	validates :allocated_money, presence: true, numericality: {greater_than_or_equal_to: 0}
 end
--- app/models/position.rb
+++ app/models/position.rb
@@ -1,7 +1,7 @@
 # Model for the positions
 class Position < ActiveRecord::Base
-	# Validates the presence and the length of the name colummn
-	validates :name, presence: true, length: {minimum: 2, maximum: 25}
+	# Validates the presence, the uniqueness and the length of the name colummn
+	validates :name, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 2, maximum: 25}
 
 	# Validates the presence and the numericality of the allocated_money column
 	validates :allocated_money, presence: true, numericality: {greater_than_or_equal_to: 0}
