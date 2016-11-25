40bc76e0264144c38b378cde505bc42e2e75ed4c
armandnaregian <armandnaregian@hotmail.com>
2016-11-25 14:13:40 -0500

    added temporary admin checkbox

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,18 +8,18 @@ class UsersController < ApplicationController
 	end
 	
 	def new
-		@user = User.new
+			@user = User.new
 	end
 
 	def create
-		@user = User.new(user_params)
-		if @user.save
-			session[:user_id] = @user.id
-			flash[:success] = "sign up successful"
-			redirect_to root_path
-		else
-			render 'new'
-		end
+			@user = User.new(user_params)
+			if @user.save
+				session[:user_id] = @user.id
+				flash[:success] = "sign up successful"
+				redirect_to root_path
+			else
+				render 'new'
+			end
 	end
 
 	def edit
