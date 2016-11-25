ecb2c625c6c4c77337b2b385317c97540b86999c
armandnaregian <armandnaregian@hotmail.com>
2016-11-25 13:54:26 -0500

    add sign in upon sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
 	before_action :set_user, only: [:edit, :update, :show, :destroy]
-	before_action :require_user
+	before_action :require_user, except: [:new, :create]
 
 	def index
 		@users = User.all		
@@ -14,8 +14,9 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
+			session[:user_id] = @user.id
 			flash[:success] = "sign up successful"
-			redirect_to user_path(@user)
+			redirect_to root_path
 		else
 			render 'new'
 		end
