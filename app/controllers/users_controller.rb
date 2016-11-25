037c8eeb9c4e627fc6cf6082098c7574b5c79f6d
armandnaregian <armandnaregian@hotmail.com>
2016-11-25 15:00:23 -0500

    Finished admin implementation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -57,6 +57,7 @@ class UsersController < ApplicationController
 	def require_same_user
 		if current_user != @user and !current_user.admin?
 			flash[:danger] = "You cannot edit or delete other people's accounts"
+			redirect_to root_path
 		end
 	end
 end
