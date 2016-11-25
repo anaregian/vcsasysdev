a12e656c70b75f840c2072e5436bb918ef31ebda
armandnaregian <armandnaregian@hotmail.com>
2016-11-25 14:47:31 -0500

    Quality of life changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,7 +55,7 @@ class UsersController < ApplicationController
 	end
 
 	def require_same_user
-		if current_user != @user
+		if current_user != @user and !current_user.admin?
 			flash[:danger] = "You cannot edit or delete other people's accounts"
 		end
 	end
