fc9112be293566863278aad739b6453208c62395
armandnaregian <armandnaregian@hotmail.com>
2016-11-25 13:25:00 -0500

    added authentication methods

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,14 @@ Rails.application.routes.draw do
 	# Initializes all the routes for incomes
 	resources :incomes
 
+	# Initializes all the routes for users
 	get 'signup', to: 'users#new'
 	resources :users, except: [:new]
+
+	# Initializes all the routes for the user sessions (log in, log out)
+	get 'login', to: 'sessions#new'
+	post 'login', to: 'sessions#create'
+	delete 'logout', to: 'sessions#destroy'
 
   	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
