7c776b3570cc1d4394fdb017027c45408125f196
Sunsetium <sunsetium@gmail.com>
2016-12-22 11:33:57 -0500

    Fixed a few variable names

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     port: 587,
     domain: 'gmail.com',
-    user_name: 'youremail@domain.com'
+    user_name: 'youremail@domain.com',
     password: 'youremailpassword',
     authentication: 'plain',
     enable_starttls_auto: true
