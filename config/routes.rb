a4c082cd25ecde3f19a99728644946ebe1bfe520
armandnaregian <armandnaregian@hotmail.com>
2017-01-17 21:24:52 -0500

    moar merge fixes

--- config/routes.rb
+++ config/routes.rb
@@ -11,18 +11,15 @@ Rails.application.routes.draw do
 	resources :incomes
 	# Initializes all the routes for estimations
 	resources :estimations
-<<<<<<< HEAD
 	# Initializes all the routes for password_resets
 	resources :password_resets
 	# Initializes all the routes for admin_codes
 	resources :codes ,:path => "secret_page", except: [:show]
-=======
 	# Initializes all the routes for memos
 	resources :memos
 	# Initializes all the routes for comments
 	resources :comments
 
->>>>>>> e8e5446c0d0343e3d4649b6d8102bc2e14f9d94f
 	# Initializes all the routes for users
 	get 'signup', to: 'users#new'
 	resources :users , except: [:new] do
