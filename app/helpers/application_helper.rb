69d14cf393bf1992609d70e0cf98b8afd88dc53b
armand <armandnaregian@hotmail.com>
2018-02-25 16:53:13 +0000

    fixed progression issue

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,17 +3,23 @@ module ApplicationHelper
 	def get_progression
 		today = Date.today
 		end_date = Date.new(today.year, 6, 1)
+		
 		if today >= end_date
-			number_of_days = (today - end_date)
-		else
-			number_of_days = (today + end_date)
+			end_date = Date.new(today.year + 1, 6, 1)
 		end
-		return (number_of_days / 365.0) * 100
+
+		number_of_days = (end_date - today).abs
+		
+		puts today
+		puts end_date
+		puts number_of_days
+		
+		return 100 - ((number_of_days / 365) * 100)
 	end
 
 	def high_spending(progress)
-  	if progress > get_progression
-			return "danger"
+	  	if progress > get_progression
+				return "danger"
 		end
 	end
 
